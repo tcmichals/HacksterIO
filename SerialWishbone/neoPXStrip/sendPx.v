c0eefbb236e982e5a37d50d1a9bb285e2766231b
Tim Michals <tmichals@gmail.com>
2024-12-07 12:28:35 -0600

    Added neoPXStrip and blinktLEDBar

---
+++ SerialWishbone/neoPXStrip/sendPx.v
@@ -0,0 +1,214 @@
+`timescale 1ns / 1ns
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/26/2024 02:31:40 PM
+// Design Name: 
+// Module Name: sendPx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sendPx(
+
+    input wire axis_aclk,
+    input wire axis_reset,
+    
+    /* AXIS slave */
+    input wire [31:0] s_axis_data,
+    input wire s_axis_valid,
+    output wire s_axis_ready,
+    
+    /* output stream */
+    output wire o_serial 
+    );
+    
+/* 160_000_000 
+.00625 us = 160_000_000 
+
+
+     */ 
+     
+                        // WS2812B timings
+localparam T0H_count =     16'd64,  // .4us 
+           T1H_count =     16'd128,  // .8us
+           T0L_count =     16'd136, // .85us
+           T1L_count =     16'd72,  //  .45us
+           GAP_count =     16'd15,
+           RESET    =      16'd8800;// above 55us
+    
+
+/* SK6812
+localparam T0H_count = 16'd48,  // .3us 
+           T1H_count = 16'd96,  // .6us
+           T0L_count = 16'd144, // .9us
+           T1L_count = 16'd96,  // .6us
+           GAP_count = 16'd10,
+           RESET    =      16'd8800;// above 80us
+ */
+reg [7:0] state;
+reg ready;
+reg [15:0] counter;
+reg [31:0] shift_reg;
+reg out_bit;
+reg [3:0] bit_state;
+reg [15:0] gapTimer; 
+
+localparam IDLE_BIT_BSTATE = 4'd0,
+           WAIT_HIGH_BSTATE=4'd1,
+           WAIT_LOW_BSTATE=4'd2,
+           DONE_BIT_BSTATE=4'd3;
+            
+localparam IDLE_STATE = 8'd0,
+          DONE_STATE = 8'd33,
+          WAIT_STATE=8'd34;
+          
+initial begin
+    ready = 0;
+    state = IDLE_STATE;
+    counter = 0;
+    bit_state = IDLE_BIT_BSTATE;
+    out_bit = 0;
+ end            
+ 
+always @(posedge axis_aclk) begin
+
+    case ( state)
+        DONE_STATE: begin
+            $display("DONE_STATE ");
+            bit_state <= IDLE_BIT_BSTATE;
+            out_bit <= 1'b0;
+            counter <= 0;
+        end
+        WAIT_STATE: begin
+            $display("WAIT_STATE ");
+            bit_state <= IDLE_BIT_BSTATE;
+            out_bit <= 1'b0;
+            counter <= 0;
+        end
+
+        IDLE_STATE: begin
+            $display("IDLE_STATE ");
+            bit_state <= IDLE_BIT_BSTATE;
+            out_bit <= 1'b0;
+            counter <= 0;
+        end
+
+        default: begin
+             $display("DEFAULT ");
+            case (bit_state)
+                IDLE_BIT_BSTATE: begin
+                    if (shift_reg[31] == 1)
+                        counter <= T1H_count;
+                    else 
+                        counter <= T0H_count;
+                    bit_state <= WAIT_HIGH_BSTATE;
+                end
+                WAIT_HIGH_BSTATE: begin     
+                    $display("counter %d", counter);     
+                    out_bit <= 1'b1;
+                    counter <= counter - 1'b1;
+                    if (counter == 0) begin
+                        out_bit <= 1'b0;
+                        bit_state <= WAIT_LOW_BSTATE;
+                        if (shift_reg[31] == 1)
+                            counter <= T1L_count;
+                        else 
+                            counter <= T0L_count;
+                    end
+                    else
+                         out_bit <= 1'b1;
+                end
+
+                WAIT_LOW_BSTATE: begin
+                    out_bit <= 1'b0;  
+                    counter <= counter - 1'b1;
+                    if (counter == 0) begin  
+                        bit_state <= DONE_BIT_BSTATE;
+                    end
+                end
+
+                DONE_BIT_BSTATE:  begin 
+                    bit_state <= IDLE_BIT_BSTATE;
+                    out_bit <= 1'b0;
+                    counter <= 0;
+                end
+    
+                default: begin 
+                        bit_state <= IDLE_BIT_BSTATE;
+                        out_bit <= 1'b0;
+                        counter <= 0;
+                end
+            endcase 
+        end
+
+
+    endcase
+  
+ end  
+                      
+always @(posedge axis_aclk) begin
+    
+    if (axis_reset) begin
+        state <= IDLE_STATE;
+        ready <= 0;
+        shift_reg <= 0;
+        gapTimer <= 0;
+    end
+    else begin 
+
+        case(state)
+        IDLE_STATE: begin
+            gapTimer <= 0;
+            if ( s_axis_valid & ready) begin
+                ready <= 0;
+                shift_reg <= s_axis_data;
+                state <= state + 1'b1;
+            end
+            else begin
+                ready <= 1;
+                state <= IDLE_STATE;
+            end
+        end    
+        DONE_STATE: begin
+            ready <= 1;
+            gapTimer <= 0;
+            state <= IDLE_STATE;
+        end
+        WAIT_STATE: begin
+            ready <= 0;
+            if(gapTimer== GAP_count)
+                state <= IDLE_STATE;
+            else
+                gapTimer<= gapTimer + 1'b1;
+
+        end
+        default: begin
+            ready <= 0;
+            if( bit_state == DONE_BIT_BSTATE) begin
+                shift_reg <= { shift_reg[30:0], 1'b0};
+                state <= state +1;
+            end
+        end
+        endcase
+    end
+
+end
+
+assign s_axis_ready = ready;
+assign o_serial = (bit_state!= IDLE_BIT_BSTATE)?out_bit:1'b0;
+
+endmodule
+
+
---
+++ SerialWishbone/neoPXStrip/wb_neoPx.v
@@ -0,0 +1,187 @@
+module wb_neoPx
+#( 
+    parameter DATA_WIDTH = 32,                  // width of data bus in bits (8, 16, 32, or 64)
+    parameter ADDR_WIDTH = 32,                  // width of address bus in bits
+    parameter SELECT_WIDTH = (DATA_WIDTH/8)     // width of word select bus (1, 2, 4, or 8)
+)
+(   input wire i_clk,
+    input wire i_rst,
+
+     // master side
+    input  wire [ADDR_WIDTH-1:0]   wb_adr_i,   // ADR_I() address
+    input  wire [DATA_WIDTH-1:0]   wb_dat_i,   // DAT_I() data in
+    output wire [DATA_WIDTH-1:0]   wb_dat_o,   // DAT_O() data out
+    input  wire                    wb_we_i,    // WE_I write enable input
+    input  wire [SELECT_WIDTH-1:0] wb_sel_i,   // SEL_I() select input
+    input  wire                    wb_stb_i,   // STB_I strobe input
+    output wire                    wb_ack_o,   // ACK_O acknowledge output
+    output wire                    wb_err_o,   // ERR_O error output
+    output wire                    wb_rty_o,   // RTY_O retry output
+    input  wire                    wb_cyc_i,   // CYC_I cycle input
+
+    output wire [31:0] m_axis_data,
+    output wire m_axis_valid,
+    input wire s_axis_ready   );
+
+reg ack,update;
+reg  [31:0] ledData[7:0];
+reg [4:0] state;
+
+reg tvalid;
+reg [31:0] o_data;
+reg sendState;
+reg [3:0] count;
+
+
+
+
+localparam LEDCOUNT = 4'd8,
+            IDLE = 4'd0,
+            DONE = LEDCOUNT +1;
+initial begin
+
+    count  = 0;
+    ack =0;
+    update = 0; 
+    tvalid = 0;
+    sendState = 0;
+    state = 0;
+    ledData[0] = 32'h0000_0000;
+    ledData[1] = 32'h0000_0000;
+    ledData[2] = 32'h0000_0000;
+    ledData[3] = 32'h0000_0000;
+    ledData[4] = 32'h0000_0000;
+    ledData[5] = 32'h0000_0000;
+    ledData[6] = 32'h0000_0000;
+    ledData[7] = 32'h0000_0000;
+    o_data = 32'd0;
+end
+
+assign  wb_ack_o = ack;
+
+ always @(*) begin
+    if ((~ack & wb_cyc_i & wb_stb_i)) begin
+        case(wb_adr_i[5:0])
+            //set all leds
+           6'h00 :   o_data = ledData[0];
+           6'h04 :   o_data = ledData[0];
+           6'h08 :   o_data = ledData[0];
+           6'h0C :   o_data = ledData[0];
+           6'h10 :   o_data = ledData[0];
+           6'h14 :   o_data = ledData[0];
+           6'h18 :   o_data = ledData[0];
+           6'h1C :   o_data = ledData[0];
+           default: o_data = 32'hFFFF_FFFF;       
+        endcase
+    end
+    else
+        o_data = 32'hFFFF_FFFF;    
+ end
+
+always @(posedge i_clk) begin
+
+    if (i_rst) begin
+        ack <= 1'b0;
+    end 
+    else begin
+
+        if ((~ack & wb_cyc_i & wb_stb_i)) begin
+            if (wb_we_i) begin
+                case(wb_adr_i[5:0])
+                 
+                //set all leds
+                6'h00 :   ledData[0]<= wb_dat_i;
+                6'h04 :   ledData[1]<= wb_dat_i;
+                6'h08 :   ledData[2]<= wb_dat_i;
+                6'h0C :   ledData[3]<= wb_dat_i;
+                6'h10 :   ledData[4]<= wb_dat_i;
+                6'h14 :   ledData[5]<= wb_dat_i;
+                6'h18 :   ledData[6]<= wb_dat_i;
+                6'h1C :   ledData[7]<= wb_dat_i;
+                default: begin 
+                      update <= 1;
+                end
+                       
+                endcase
+            end 
+
+            ack <= 1'b1;     
+        end
+        else if (state == DONE && update)
+            update <= 0;
+
+        if (ack) begin
+            ack <= 1'b0;
+        end
+        
+    end
+    
+end
+
+
+
+always @(posedge i_clk) begin
+
+    case(state) 
+        IDLE: begin 
+                if ( update)
+                    state<= state + 1'b1;
+                else
+                   state <= 0;
+
+                tvalid <= 0;
+                sendState <= 0;
+
+                count <= 0;
+        end
+
+        DONE: begin 
+                state <= IDLE;
+                tvalid <= 0;
+                sendState <= 0;
+
+                count <= 0;
+        end
+
+        default: begin
+            if ( sendState == 0) 
+            begin
+                if(s_axis_ready) begin
+                    tvalid <= 1'b1;
+                    sendState <= 1;
+                    count <= count;
+                    state <= state;
+
+                end 
+                else begin
+                    tvalid <= 0;
+                    count <= count;
+                    state <= state;
+                    sendState <= sendState;
+                end
+            end 
+            else begin
+                tvalid <= 0;
+                sendState <= 0;
+                if (count < LEDCOUNT)
+                    count <= count + 1'b1;
+                else
+                    count <= count;
+                state <= state + 1'b1;
+    
+            end
+
+         end  
+       
+     endcase
+end
+
+//always return led settings 
+assign wb_dat_o= { 2'd0, tvalid, state, o_data[23:0] };
+assign wb_err_o = 0;
+assign wb_rty_o = 0;
+assign m_axis_data = (count < LEDCOUNT)?ledData[count]:0;
+assign m_axis_valid = tvalid;
+
+
+endmodule
